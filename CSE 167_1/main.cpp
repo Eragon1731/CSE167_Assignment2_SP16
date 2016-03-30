f52c7a11c11b8636c672c30a6d3f6904ec78c596
Christy Ye <Cocona2121@Christys-MacBook-Pro.local>
2016-03-30 14:25:17 -0700

    Initial Commit

---
+++ CSE 167_1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CSE 167_1
+//
+//  Created by Christy Ye on 3/30/16.
+//  Copyright (c) 2016 Christy Ye. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
